1b6481ffa1ba2b64269bd21c4434dc8e3723bd33
ezi-s-u <ljsgoo0617@gmail.com>
2024-02-06 13:24:29 +0900

    [feat] DB 연결 및 회원가입 API 완성

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 // const로 상수 선언 
 const sequelize = require('../config/config')
 
-const User = require('./user')(sequelize);// sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄
+const User = require('./user')(sequelize); // sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄
 
 module.exports = {
   User,
