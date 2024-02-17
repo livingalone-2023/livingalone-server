6682df886ab36e419b618f7f2c2ec108dcf8a93e
ezi-s-u <ljsgoo0617@gmail.com>
2024-02-17 23:59:49 +0900

    [feat] 질문 업로드 api

--- routes/question.js
+++ routes/question.js
@@ -6,5 +6,21 @@ const crypto = require('crypto');
 
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+  try {
+    const question = await Question.create(req.body);
+
+    if(question) {
+      return res.status(200).json( { "message" : "질문 업로드 성공"} )
+    } else {
+      return res.status(400).json( { "message" : "질문 업로드 실패"} )
+    }
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json( { "message" : "질문 업로드 실패"} )
+  }
+})
+
 
 module.exports = router
