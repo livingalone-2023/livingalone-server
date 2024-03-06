0b501c267baa91643fa191c3b3cbc48e03922017
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-03-06 14:28:06 +0900

    feat : 답변 작성 api

--- routes/answer.js
+++ routes/answer.js
@@ -6,5 +6,45 @@ const crypto = require('crypto');
 
 const router = express.Router();
 
+//답변 작성 api
+router.post('/', async (req, res) => {
+    try {
+        const answer = await Answer.create(req.body); // 요청의 내용으로 새로운 답변 생성
+        if (answer) {
+            return res.status(200).json({ "message": "답변이 성공적으로 등록되었습니다." });
+        } else {
+            return res.status(400).json({ "message": "답변 등록에 실패했습니다." });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ "message": "서버 오류로 답변 등록에 실패했습니다." });
+    }
+})
 
+//답변 수정 api 
+router.patch('/:answer_id',async(req,res)=>{
+const {isAccepted,isLiked}=req.body
+const id=req.params.answer_id
+try{
+    const answer=await Answer.update({
+        isAccepted:isAccepted,
+        isLiked:isLiked
+    },{
+        where:{id:id}
+    })
+    const editedAnswer=await Answer.findOne({
+        where :{id:id}
+    })
+    console.log("***",editedAnswer.dataValues)
+    if(answer){
+        return res.status(201).json({"message":"답변 수정이 정상적으로 되었습니다."})
+    }else{
+        return res.status(404).json({"message":"답변 수정이 정상적으로 실패하였습니다."})
+    }
+}catch(error){
+    console.log(error);
+    return res.status(500).json({"message":"답변 수정이 정상적으로 실패하였습니다."})
+    //console.log(error);
+}
+})
 module.exports = router
