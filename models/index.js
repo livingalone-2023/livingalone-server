75b9cf656cf601e1d2b1770a7ec2d5db42cc8415
ezi-s-u <ljsgoo0617@gmail.com>
2024-04-24 11:48:41 +0900

    [feat] 모델(테이블) 관계 설정

--- models/index.js
+++ models/index.js
@@ -8,7 +8,19 @@ const Answer = require('./answer')(sequelize);
 const Like = require('./like')(sequelize);
 
 User.hasMany(Question, { foreignKey: 'user_pk' });
-Question.belongsTo(User, {foreignKey : "user_pk"});
+User.hasMany(Like, { foreignKey: 'user_pk'});
+// User.hasMany(Answer, { foreignKey: 'user_pk'});
+
+Question.belongsTo(User, {foreignKey : 'user_pk'});
+Question.hasMany(Answer, {foreignKey : 'question_pk'});
+
+Answer.belongsTo(User, {foreignKey : 'user_pk'});
+// Answer.belongsTo(Question, {foreignKey : 'question_pk'});
+Answer.hasMany(Like, {foreignKey : 'answer_pk'});
+
+
+Like.belongsTo(User, {foreignKey: 'user_pk'});
+Like.belongsTo(Answer, {foreignKey : 'answer_pk'});
 
 module.exports = {
   User,
