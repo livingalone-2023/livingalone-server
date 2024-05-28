e045fbf9c2fa2785af27fbf2758c764d1623ed6c
ezi-s-u <ljsgoo0617@gmail.com>
2024-05-28 20:53:52 +0900

    [refactor] 좋아요 관련 모델, 테이블 관계 설정 코드 삭제

--- models/index.js
+++ models/index.js
@@ -5,27 +5,20 @@ const sequelize = require('../config/config')
 const User = require('./user')(sequelize); // sequelize가 db 정보를 가지고 있으므로 전달해주면 얘가 만들어봄
 const Question = require('./question')(sequelize);
 const Answer = require('./answer')(sequelize);
-const Like = require('./like')(sequelize);
 
 User.hasMany(Question, { foreignKey: 'user_pk' });
-User.hasMany(Like, { foreignKey: 'user_pk'});
-// User.hasMany(Answer, { foreignKey: 'user_pk'});
+User.hasMany(Answer, { foreignKey: 'user_pk'});
 
 Question.belongsTo(User, {foreignKey : 'user_pk'});
 // Question.hasMany(Answer, {foreignKey : 'question_pk'});
 
 Answer.belongsTo(User, {foreignKey : 'user_pk'});
 // Answer.belongsTo(Question, {foreignKey : 'question_pk'});
-Answer.hasMany(Like, {foreignKey : 'answer_pk'});
 
 
-Like.belongsTo(User, {foreignKey: 'user_pk'});
-Like.belongsTo(Answer, {foreignKey : 'answer_pk'});
 
 module.exports = {
   User,
   Question,
-  Answer,
-  Like,
-  sequelize
+  Answer
 }
